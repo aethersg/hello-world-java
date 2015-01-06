763939261f133b0ba8978c63dcfd02d7039948b1
jude <jude@affle.com>
2015-01-06 23:50:56 +0800

    removed unwanted comments

--- hello-world/src/hello/world/HelloWorld.java
+++ hello-world/src/hello/world/HelloWorld.java
@@ -1,16 +1,9 @@
 package hello.world;
 
-/**
- *
- * @author judetan
- */
 public class HelloWorld {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-       System.out.println("Hello World");
+        System.out.println("Hello World");
     }
-    
+
 }
